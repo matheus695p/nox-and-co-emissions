4fcee1e00fc3e71bc5d8bcce02723cd55794d4ed
matheus695p <mateu_695p@hotmail.com>
2021-04-10 13:28:31 -0400

    add: utils y nuevos config de las redes

---
+++ src/config_nn.py
@@ -0,0 +1,30 @@
+import warnings
+from argparse import ArgumentParser
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+def arguments_parser():
+    """
+    El parser de argumentos de parámetros que hay que setiar para entrenar
+    una red deep renewal
+    Returns
+    -------
+    args : argparser
+        argparser con todos los parámetros del modelo.
+    """
+    # argumentos
+    parser = ArgumentParser()
+    parser.add_argument(
+        "-f", "--fff", help="haciendo weon a python", default="1")
+    # agregar donde correr y guardar datos
+    parser.add_argument('--batch_size', type=int, default=1024)
+    parser.add_argument('--epochs', type=int, default=500)
+    parser.add_argument('--patience', type=int, default=80)
+    parser.add_argument('--min_delta', type=float, default=1e-8)
+    parser.add_argument('--optimizer', type=str, default="adam")
+    parser.add_argument('--lr_factor', type=float, default=0.75)
+    parser.add_argument('--lr_patience', type=int, default=15)
+    parser.add_argument('--lr_min', type=float, default=1e-4)
+    parser.add_argument('--validation_size', type=float, default=0.2)
+    args = parser.parse_args()
+    return args
